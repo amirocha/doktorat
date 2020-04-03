0ff218e47b03c8ab5bb26e0354537d2848fc705b
amirocha <mirochagnieszka@gmail.com>
2020-04-03 03:27:23 +0200

    poprawki kwiecien 2020

---
+++ uv/ngc1333/profiles/ngc1333_1-3_ave_spectra_vel_resam.py
@@ -0,0 +1,400 @@
+'''
+DESCRIPTION: This script displays the resampled profiles (0.5 km/s)
+DESCRIPTION: of diff. molecules for 14 diff. positions in SERPENS
+
+The averaged region is consistent with HCN 1-0 beam size (27.8"), 
+because it's the biggest for this molecule. We used the same 
+beam size for other molecules.
+'''
+
+#!/usr/bin/python3.3
+
+# name the output file
+psname = 'ngc1333_spectra_1.eps'
+
+# import packages
+from numpy import *
+from pylab import *
+import matplotlib.pyplot as plt
+from matplotlib import *
+
+fig = plt.figure(figsize = (20,10), dpi = 400)
+
+
+
+#plt.rcParams["font.family"] = "Times New Roman"
+rc('font', **{'family':'serif', 'serif':['Times New Roman']})
+params = {'backend': 'pdf',
+          #'axes.labelsize': 12,
+          #'text.fontsize': 12,
+          #'legend.fontsize': 12,
+          #'xtick.labelsize': 7,
+          #'ytick.labelsize': 7,
+          # The comm. below determines whether you use LaTeX 
+          # for all text in matplotlib (you probably don't want 
+          # to turn this on, but may)
+          'text.usetex': False,
+          # four comm. below (math) determines what is used for math rendering 
+          'mathtext.rm': 'serif',
+          'mathtext.it': 'serif:italic',
+          'mathtext.bf': 'serif:bold',
+          'mathtext.fontset': 'custom',
+          #'figure.figsize': fig_size,
+          'axes.unicode_minus': True}
+matplotlib.rcParams.update(params)
+
+plt.tick_params(axis='y',          # changes apply to the y-axis only
+    which='both',      # both major and minor ticks are affected
+    right=False)      # ticks along the bottom edge are off
+            # ticks along the top edge are off  x-axis:bottom/top=False
+
+""" FIRST PANEL - READ INPUT DATA
+########## C34S 3-2 ##########
+"""
+v_c34s32_p1, Tmb_c34s32_p1 = loadtxt('./ngc1333_c34s32_I2A.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## CS 3-2 ##########
+"""
+v_cs32_p1, Tmb_cs32_p1 = loadtxt('./ngc1333_cs32_I2A.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## H13CN 1-0 ##########
+"""
+v_h13cn10_p1, Tmb_h13cn10_p1 = loadtxt('./ngc1333_h13cn10_I2A.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+##########  HCN 1-0 ##########
+"""
+v_hcn10_p1, Tmb_hcn10_p1 = loadtxt('./ngc1333_hcn10_I2A.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## CN 1-0 ##########
+"""
+v_cn10_p1, Tmb_cn10_p1 = loadtxt('./ngc1333_cn10_I2A.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+ax1 = plt.subplot(1, 3, 1)
+ax2 = plt.subplot(1, 3, 2)
+ax3 = plt.subplot(1, 3, 3)
+
+
+# remove empty space between subplots
+#fig.subplots_adjust(hspace=.1) # height spaces
+fig.subplots_adjust(wspace=0) # width spaces
+
+
+
+"""
+FIRST PANEL - CREATE A PLOT
+"""
+#ax1.set_xlabel(r'$\mathrm{V_{LSR}\;[km/s]}$', fontsize = 14)
+ax1.set_ylabel(r'$T_\mathrm{MB}$ [K] + Offset', fontsize = 14)
+
+
+# major x ticks every 20, minor ticks every 10
+# major y ticks every 1, minor ticks every 0.5 
+major_ticks_x = np.arange(-80, 35, 20)                                              
+minor_ticks_x = np.arange(-80, 35, 10) 
+
+major_ticks_y = np.arange(0.0, 32, 4.0)                                              
+minor_ticks_y = np.arange(0.0, 32, 0.5) 
+
+ax1.set_xticks(major_ticks_x)                                                       
+ax1.set_xticks(minor_ticks_x, minor=True)
+
+ax1.set_yticks(major_ticks_y)                                                       
+ax1.set_yticks(minor_ticks_y, minor=True)
+
+# Set the tick labels font
+for label in (ax1.get_xticklabels() + ax1.get_yticklabels()):
+#    label.set_fontname('Arial')
+    label.set_fontsize(12)
+
+ax1.axvline(x=7.7, linewidth=0.7)
+
+
+
+"""
+########## C34S 3-2 (x 3) ##########
+"""
+ax1.plot(v_c34s32_p1, 3*(Tmb_c34s32_p1) + 16.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax1.axhline(y=16, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1.0, linestyle = '-')
+
+"""
+########## CS 3-2 ##########
+"""
+ax1.plot(v_cs32_p1, Tmb_cs32_p1 + 12.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax1.axhline(y=12, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1.0, linestyle = '-')
+
+"""
+########## H13CN 1-0 ##########
+"""
+ax1.plot(v_h13cn10_p1, 5*(Tmb_h13cn10_p1) + 8.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax1.axhline(y=8, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+"""
+########## HCN 1-0 ##########
+"""
+ax1.plot(v_hcn10_p1, Tmb_hcn10_p1 + 4.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax1.axhline(y=4, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+"""
+########## CN 1-0 ##########
+"""
+ax1.plot(v_cn10_p1, Tmb_cn10_p1, color = 'black', linewidth=1.0, linestyle = '-')
+ax1.axhline(y=0, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+ax1.annotate(r'$\mathrm{C^{34}S\;\;3-2\;(\times3)}$', fontsize=12, xy=(-85.0, 18.0), textcoords='data')
+ax1.annotate(r'$\mathrm{CS\;\;3-2}$', fontsize=12, xy=(-85.0, 14.), textcoords='data')
+ax1.annotate(r'$\mathrm{H^{13}CN\;\;1-0\;(\times5)}$', fontsize=12, xy=(-85.0, 10.0), textcoords='data')
+ax1.annotate(r'$\mathrm{HCN\;\;1-0}$', fontsize=12, xy=(-85.0, 5.5), textcoords='data')
+ax1.annotate(r'$\mathrm{CN\;\;1-0}$', fontsize=12, xy=(-85.0, 1.7), textcoords='data')
+ax1.annotate(r'$\mathrm{IRAS\;2A}$', color='blue', fontsize=18, xy=(-30.0, 20), textcoords='data')
+
+
+
+# the upper and lower axis limits on a LEFT GRAPH
+ax1.set_xlim([-90.0, 40.0])
+ax1.set_ylim([-0.5, 22.5])
+
+
+
+
+
+"""
+***************************************************************
+***************************************************************
+***************************************************************
+"""
+
+
+
+
+
+""" SECOND PANEL - READ INPUT DATA
+########## C34S 3-2 ##########
+"""
+v_c34s32_p2, Tmb_c34s32_p2 = loadtxt('./ngc1333_c34s32_I2B.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+##########  CS 3-2  ##########
+"""
+v_cs32_p2, Tmb_cs32_p2 = loadtxt('./ngc1333_cs32_I2B.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## H13CN 1-0 ##########
+"""
+v_h13cn10_p2, Tmb_h13cn10_p2 = loadtxt('./ngc1333_h13cn10_I2B.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## HCN 1-0 ##########
+"""
+v_hcn10_p2, Tmb_hcn10_p2 = loadtxt('./ngc1333_hcn10_I2B.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## CN 1-0 ##########
+"""
+v_cn10_p2, Tmb_cn10_p2 = loadtxt('./ngc1333_cn10_I2B.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+
+"""
+SECOND PANEL - CREATE A PLOT
+"""
+#ax2.set_xlabel(r'$\mathrm{V_{LSR}\;[km/s]}$', fontsize = 14)
+
+
+# major x ticks every 20, minor ticks every 10
+# major y ticks every 1, minor ticks every 0.5 
+major_ticks_x = np.arange(-80, 35, 20)                                              
+minor_ticks_x = np.arange(-80, 35, 10) 
+
+major_ticks_y = ([])                                              
+minor_ticks_y = ([])  
+
+ax2.set_xticks(major_ticks_x)                                                       
+ax2.set_xticks(minor_ticks_x, minor=True)
+
+ax2.set_yticks(major_ticks_y)                                                       
+ax2.set_yticks(minor_ticks_y, minor=True)
+
+# Set the tick labels font
+for label in (ax2.get_xticklabels() + ax2.get_yticklabels()):
+#    label.set_fontname('Arial')
+    label.set_fontsize(12)
+
+ax2.axvline(x=7.7, linewidth=0.7)
+
+"""
+########## C34S 3-2 (x 3) ##########
+"""
+ax2.plot(v_c34s32_p2, 3*(Tmb_c34s32_p2) + 16.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax2.axhline(y=16, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+"""
+########## CS 3-2 ##########
+"""
+ax2.plot(v_cs32_p2, Tmb_cs32_p2 + 12.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax2.axhline(y=12, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1.0, linestyle = '-')
+
+"""
+########## H13CN 1-0 (x 5) ##########
+"""
+ax2.plot(v_h13cn10_p2, 5*(Tmb_h13cn10_p2) + 8.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax2.axhline(y=8, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1.0, linestyle = '-')
+
+"""
+########## HCN 1-0 ##########
+"""
+ax2.plot(v_hcn10_p2, Tmb_hcn10_p2 + 4.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax2.axhline(y=4, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1.0, linestyle = '-')
+
+"""
+########## CN 1-0 ##########
+"""
+ax2.plot(v_cn10_p2, Tmb_cn10_p2, color = 'black', linewidth=1.0, linestyle = '-')
+ax2.axhline(y=0, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1.0, linestyle = '-')
+
+
+ax2.annotate(r'$\mathrm{C^{34}S\;\;3-2\;(\times3)}$', fontsize=12, xy=(-85.0, 18.0), textcoords='data')
+ax2.annotate(r'$\mathrm{CS\;\;3-2}$', fontsize=12, xy=(-85.0, 14), textcoords='data')
+ax2.annotate(r'$\mathrm{H^{13}CN\;\;1-0\;(\times5)}$', fontsize=12, xy=(-85.0, 10.0), textcoords='data')
+ax2.annotate(r'$\mathrm{HCN\;\;1-0}$', fontsize=12, xy=(-85.0, 5.5), textcoords='data')
+ax2.annotate(r'$\mathrm{CN\;\;1-0}$', fontsize=12, xy=(-85.0, 1.7), textcoords='data')
+ax2.annotate(r'$\mathrm{IRAS\;2B}$', color='blue', fontsize=18, xy=(-30.0, 20), textcoords='data')
+
+
+
+# the upper and lower axis limits on a RIGHT GRAPH
+ax2.set_xlim([-90.0, 40.0])
+ax2.set_ylim([-0.5, 22.5])
+
+
+
+
+
+"""
+***************************************************************
+***************************************************************
+***************************************************************
+"""
+
+
+
+
+
+""" THIRD PANEL - READ INPUT DATA
+##########  C34S 3-2 ##########
+"""
+v_c34s32_p3, Tmb_c34s32_p3 = loadtxt('./ngc1333_c34s32_HRF65.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## CS 3-2 ##########
+"""
+#v_cs32_p3, Tmb_cs32_p3 = loadtxt('./ngc1333_cs32_HRF65.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## H13CN 1-0 ##########
+"""
+v_h13cn10_p3, Tmb_h13cn10_p3 = loadtxt('./ngc1333_h13cn10_HRF65.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## HCN 1-0 ##########
+"""
+v_hcn10_p3, Tmb_hcn10_p3 = loadtxt('./ngc1333_hcn10_HRF65.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+########## CN 1-0 ##########
+"""
+v_cn10_p3, Tmb_cn10_p3 = loadtxt('./ngc1333_cn10_HRF65.txt', usecols=(0, 1), unpack=True, skiprows=1)
+
+
+"""
+THIRD PANEL - CREATE A PLOT
+"""
+
+#ax3.set_xlabel(r'$\mathrm{V_{LSR}\;[km/s]}$', fontsize = 14)
+
+
+# major x ticks every 20, minor ticks every 10
+# major y ticks every 1, minor ticks every 0.5 
+major_ticks_x = np.arange(-80, 35, 20)                                              
+minor_ticks_x = np.arange(-80, 35, 10) 
+
+major_ticks_y = ([])                                              
+minor_ticks_y = ([])
+
+ax3.set_xticks(major_ticks_x)                                                       
+ax3.set_xticks(minor_ticks_x, minor=True)
+
+ax3.set_yticks(major_ticks_y)                                                       
+ax3.set_yticks(minor_ticks_y, minor=True)
+
+# Set the tick labels font
+for label in (ax3.get_xticklabels() + ax3.get_yticklabels()):
+#    label.set_fontname('Arial')
+    label.set_fontsize(12)
+
+ax3.axvline(x=7.7, linewidth=0.7)
+
+"""
+########## C34S 3-2 (x 3) ##########
+"""
+ax3.plot(v_c34s32_p3, 3*(Tmb_c34s32_p3) + 16.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax3.axhline(y=16, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+
+"""
+########## CS 3-2 ##########
+"""
+#ax3.plot(v_cs32_p3, Tmb_cs32_p3 + 12.0, color = 'black', linewidth=1.0, linestyle = '-')
+#ax3.axhline(y=12, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+"""
+########## H13CN 1-0 (x 5) ##########
+"""
+ax3.plot(v_h13cn10_p3, 5*(Tmb_h13cn10_p3) + 8.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax3.axhline(y=8, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+"""
+########## HCN 1-0 ##########
+"""
+ax3.plot(v_hcn10_p3, (Tmb_hcn10_p3)/2 + 4.0, color = 'black', linewidth=1.0, linestyle = '-')
+ax3.axhline(y=4, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+"""
+########## CN 1-0 ##########
+"""
+ax3.plot(v_cn10_p3, Tmb_cn10_p3, color = 'black', linewidth=1.0, linestyle = '-')
+ax3.axhline(y=0, xmin = -90.0, xmax = 40.0, color = 'green', linewidth=1, linestyle = '-')
+
+
+ax3.annotate(r'$\mathrm{C^{34}S\;\;3-2\;(\times3)}$', fontsize=12, xy=(-85.0, 18.0), textcoords='data')
+#ax3.annotate(r'$\mathrm{CS\;\;3-2}$', fontsize=12, xy=(-85.0, 14.), textcoords='data')
+ax3.annotate(r'$\mathrm{H^{13}CN\;\;1-0\;(\times5)}$', fontsize=12, xy=(-85.0, 10.0), textcoords='data')
+ax3.annotate(r'$\mathrm{HCN\;\;1-0}$', fontsize=12, xy=(-85.0, 5.5), textcoords='data')
+ax3.annotate(r'$\mathrm{CN\;\;1-0}$', fontsize=12, xy=(-85.0, 1.7), textcoords='data')
+ax3.annotate(r'$\mathrm{HRF\;65}$', color='blue', fontsize=18, xy=(-30.0, 20), textcoords='data')
+
+
+# the upper and lower axis limits on a RIGHT GRAPH
+ax3.set_xlim([-90.0, 40.0])
+ax3.set_ylim([-0.5, 22.5])
+
+
+# close and save file
+savefig(psname, format = 'eps', bbox_inches = 'tight') 
+clf()
--- uv/serpens/cn_hcn_statistics.py
+++ uv/serpens/cn_hcn_statistics.py
@@ -3,16 +3,20 @@ import statistics
 
 
 SMM = [0.61, 0.65, 0.85, 0.47, 0.67, 0.81, 0.43, 0.24, 0.41, 0.71]
+SMM_wings = [0.4, 0.53, 0.4, 0.57, 0.31, 0.55, 0.27, 0.11, 0.15, 0.63]
 
 outflow = [0.58, 0.51, 0.13, 0.25, 0.56]
+outflow_wings = [0.24, 0.47, 0.13, 0.03, 0.49]
 
 class0 = [0.61, 0.65, 0.85, 0.43, 0.24]
+class0_wings = [0.4, 0.53, 0.57, 0.27, 0.11]
 class1 = [0.47, 0.67, 0.81, 0.41, 0.71]
+class1_wings = [0.4, 0.31, 0.55, 0.15, 0.63]
 
 #early = [0.65, 0.38, 0.94, 0.67, 0.81] #early class0 only
 #late = [0.60, 0.89, 0.37, 0.68, 0.56] #late class0 and class 1
 
-lists = [SMM, outflow, class0, class1]
+lists = [SMM_wings, outflow_wings, class0_wings, class1_wings]
 lists_names = ['SMM', 'outflow', 'class0', 'class1']
 
 def means(data):
@@ -27,7 +31,7 @@ def standard_deviations(data):
 def main():  
 
 	integrals=[]
-	file2=open('statistics.txt','w')
+	file2=open('statistics.txt','a')
 	file2.write('Sample     Mean     Median    Standard deviation\n')
 	for i in range(len(lists)):
 		mean = means(lists[i])
